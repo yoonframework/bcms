1dbcc2e9edc23e84dad745f2d35793a8c34ad2c1
yoonieKing <rldbs0812@gmail.com>
2022-03-16 23:52:28 +0900

    First Commit

---
+++ src/test/java/com/yoonframework/bcms/BcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoonframework.bcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
